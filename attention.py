3d4d05a9227624e00fde032fdbc3107226cfeaf6
Shehryar Ashraf <shehryarashraf99@gmail.com>
2025-05-23 09:09:43 +0500

    initialized 3 files
    I have initialized 3 files, attention.py encoder.py and decoder.py. code is work in process

---
+++ attention.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+import math
+
+
+
+## we will have to learn to code transformer architecture to make self attention class
+class SelfAttention(nn.Module):
